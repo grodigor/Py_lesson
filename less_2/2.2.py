ba5338e4b0edb1405e0adced52c73739b56475e4
grodigor <80203405+grodigor@users.noreply.github.com>
2021-06-10 14:07:28 +0300

    2.2

---
+++ less_2/2.2.py
@@ -0,0 +1,3 @@
+num = int(input('Please enter an integer number: '))
+print('The next number for the number', num, 'is', str(num + 1) + '.', '\nThe previous number for the number', num,
+      'is', str(num - 1) + '.')
